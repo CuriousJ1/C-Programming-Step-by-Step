b078597a31f16c572675be6733dc0416e557d835
JJ <takhar200@gmail.com>
2023-05-28 21:47:56 +1000

    refactoring code

--- loops/smart_looking.c
+++ loops/smart_looking.c
@@ -7,11 +7,14 @@ int main()
     printf("Enter n: ");
     scanf("%d", &n);
 
-    // for (initalise (frist thing in loop) which can be more than 1, 
-    // termination condition, modifier)
-    // you can have more than one modifer by extending;
+    // for (initalise (frist thing in loop) which can be more than 1,
+    // then you have a condition
+    // then goes to the code in the for loop
+    // then goes to the modifier
+
     // notice the semi colons in this
-    for (sum = 0, i = 1; i <= n; sum = sum + i, i++);
+
+    for (sum = 0, i = 1; i <= n; sum += i, i++);
 
     printf("sum of first %d natural numbers is %d\n", n, sum);
 
