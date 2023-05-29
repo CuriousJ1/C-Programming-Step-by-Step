25d1bbd2342acf9a942d1c4d8a3cb3591a625a2e
JJ <takhar200@gmail.com>
2023-05-29 16:53:57 +1000

    Completion of loops module

--- loops/random.c
+++ loops/random.c
@@ -4,15 +4,17 @@
 
 int main()
 {
-
     int k, i;
     long t;
     t = time(NULL);
 
+    // to get random numbers continuouly, you must change the seed in srand
+    //  time is constantly different
     srand(t);
-    for (i = 1, i < 10; i++) {
+    for (i = 1; i < 10; i++) {
         k = rand() % 1000;
         printf("%d\n", k);
     }
+    
     return 0;
 }
